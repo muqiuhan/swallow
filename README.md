3563f57a184258d077f010ad23cce6daf29aa4e7
Muqiu Han <muqiu-han@outlook.com>
2022-07-31 07:27:11 +0800

    Make the standard library load automatically

--- README.md
+++ README.md
@@ -123,22 +123,14 @@ Result:
 ## From source
   Since this project is developed using OCaml5, you need to install the OCaml5 environment. The current latest OCaml5 Release version is OCaml5.0.0~alpha1.You can install this version via `opam update && opam switch create 5.0.0~alpha1 --repositories=default,beta=git+https://github.com/ocaml/ocaml-beta-repository.git` to install this version of OCaml environment.
 
-- Dependencies
-  
-  This project is built with dune, you can install it with `opam install dune`
-  
-- Build
-  
-  Run `dune build --profile release` to build release version from source (or `dune build --profile dev` for dev)
+This project is built with dune, you can install it with `opam install dune`.
 
-- Test
-  
-  Run `dune test` to run all test cases
+1. Build and install the standard library with `ocaml pom.ml install`
+2. Run all tests with `ocaml pom.ml test`
+3. Execute with `mlisp`
 
-- Exec
+NOTE: Uninstall with `ocaml pom.ml uninstall`
   
-  Run `dune exec mlisp` to run the executable
-
 # License
 Copyright (C) 2022 Muqiu Han
 
--- bin/main.ml
+++ bin/main.ml
@@ -23,100 +23,41 @@ open Mlisp.Ast
 open Mlisp.Types.Reader
 open Mlisp.Types.Object
 
+let stdlib_path = "/usr/include/mlisp/stdlib.mlisp"
+
 let get_input_channel () =
   try open_in Sys.argv.(1) with
   | Invalid_argument _ -> stdin
 ;;
 
-let _stdlib =
-  let eval env e =
-    match e with
-    | Defexpr d -> eval_def d env
-    | _ -> raise (Type_error_exn "Can only have definitions in stdlib")
-  in
-  let rec slurp stm env =
-    try stm |> read_sexpr |> build_ast |> eval env |> snd |> slurp stm with
-    | Stream.Failure -> env
-  in
-  let stm =
-    make_stringstream
-      "\n\
-      \  (defun o (f g) (lambda (x) (f (g x))))\n\
-      \  (setq caar (o car car))\n\
-      \  (setq cadr (o car cdr))\n\
-      \  (setq caddr (o cadr cdr))\n\
-      \  (setq cadar (o car (o cdr car)))\n\
-      \  (setq caddar (o car (o cdr (o cdr car))))\n\n\
-      \  (setq cons pair)\n\n\
-      \  (setq newline (int->char 10))\n\
-      \  (setq space (int->char 32))\n\n\
-      \  ; This is pretty awkward looking because we have no other way to sequence\n\
-      \  ; operations. We have no begin, nothing.\n\
-      \  (defun println (s)\n\
-      \    (let ((ok (print s)))\n\
-      \      (print newline)))\n\n\
-      \  ; This is less awkward because we actually use ic and c.\n\
-      \  (defun getline ()\n\
-      \    (let* ((ic (getchar))\n\
-      \           (c (int->char ic)))\n\
-      \      (if (or (eq c newline) (eq ic ~1))\n\
-      \        empty-symbol\n\
-      \        (cat c (getline)))))\n\n\
-      \  (defun null? (xs)\n\
-      \    (eq xs '()))\n\n\
-      \  (defun length (ls)\n\
-      \    (if (null? ls)\n\
-      \      0\n\
-      \      (+ 1 (length (cdr ls)))))\n\n\
-      \  (defun take (n ls)\n\
-      \    (if (or (< n 1) (null? ls))\n\
-      \      '()\n\
-      \      (cons (car ls) (take (- n 1) (cdr ls)))))\n\n\
-      \  (defun drop (n ls)\n\
-      \    (if (or (< n 1) (null? ls))\n\
-      \      ls\n\
-      \      (drop (- n 1) (cdr ls))))\n\n\
-      \  (defun merge (xs ys)\n\
-      \    (if (null? xs)\n\
-      \      ys\n\
-      \      (if (null? ys)\n\
-      \        xs\n\
-      \        (if (< (car xs) (car ys))\n\
-      \          (cons (car xs) (merge (cdr xs) ys))\n\
-      \          (cons (car ys) (merge xs (cdr ys)))))))\n\n\
-      \  (defun mergesort (ls)\n\
-      \    (if (null? ls)\n\
-      \      ls\n\
-      \      (if (null? (cdr ls))\n\
-      \        ls\n\
-      \        (let* ((size (length ls))\n\
-      \               (half (/ size 2))\n\
-      \               (first (take half ls))\n\
-      \               (second (drop half ls)))\n\
-      \          (merge (mergesort first) (mergesort second))))))\n\
-      \  "
-  in
-  slurp stm basis
-;;
-
-let rec repl a_stream env =
-  if a_stream.stdin
-  then (
-    print_string "> ";
-    flush stdout);
-  let ast = build_ast (read_sexpr a_stream) in
-  let result, env' = eval ast env in
-  if a_stream.stdin then print_endline (string_object result);
-  repl a_stream env'
-;;
-
 let () =
+  let rec repl a_stream env =
+    if a_stream.stdin
+    then (
+      print_string "> ";
+      flush stdout);
+    let ast = build_ast (read_sexpr a_stream) in
+    let result, env' = eval ast env in
+    if a_stream.stdin then print_endline (string_object result);
+    repl a_stream env'
+  in
+  let stdlib =
+    let eval env e =
+      match e with
+      | Defexpr d -> eval_def d env
+      | _ -> raise (Type_error_exn "Can only have definitions in stdlib")
+    in
+    let rec slurp stm env =
+      try stm |> read_sexpr |> build_ast |> eval env |> snd |> slurp stm with
+      | Stream.Failure -> env
+    in
+    let stm = make_filestream (open_in stdlib_path) in
+    slurp stm basis
+  in
   let input_channel = get_input_channel () in
-  try
-    repl (make_filestream input_channel) basis
-  with
+  try repl (make_filestream input_channel) stdlib with
   | _ ->
     if input_channel = stdin
-    then repl (make_filestream input_channel) basis
+    then repl (make_filestream input_channel) stdlib
     else close_in input_channel
 ;;
