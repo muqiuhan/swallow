7a2d718998f391d644369d269a18880b057167f5
Muqiu Han <muqiu-han@outlook.com>
2024-01-10 20:27:31 +0800

    [lexer]: update

--- src/lexer/xmake.lua
+++ src/lexer/xmake.lua
@@ -2,8 +2,12 @@ target("lexer")
   set_kind("shared")
   
   before_build(function (target)
-    os.run("flex -o %s/src/lexer.cpp %s/lexer.l", os.scriptdir(), os.scriptdir())
-    os.run("clang-format -i %s/src/lexer.cpp", os.scriptdir())
+    os.run("flex -o $(scriptdir)/src/flex_lexer.cpp $(scriptdir)/lexer.l")
   end)
 
+  add_includedirs("../ast/include", "../parser/include")
   add_files("src/*.cpp")
+
+  on_clean(function (target)
+    os.run("rm $(scriptdir)/src/flex_lexer.cpp")
+  end)
