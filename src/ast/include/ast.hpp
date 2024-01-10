6c461ffc4f8323e35992c3ec696a53094b8f4684
Muqiu Han <muqiu-han@outlook.com>
2024-01-10 21:21:53 +0800

    [swallow]: Fix some code style issues and Add LICENSE header

--- src/ast/include/ast.hpp
+++ src/ast/include/ast.hpp
@@ -1,4 +1,37 @@
+// Copyright (c) 2023 Muqiu Han
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//     notice,
+//       this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Terifo nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SWALLOW_AST_HPP
+#define SWALLOW_AST_HPP
+
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace swallow::ast {
@@ -23,24 +56,24 @@ public:
   using Ptr = std::unique_ptr<Branch>;
 
 private:
-  const Pattern::Ptr Pattern;
+  const Pattern::Ptr Patt;
   const AST::Ptr Expr;
 
 public:
-  Branch(Pattern::Ptr Pattern, AST::Ptr Expr)
-      : Pattern(std::move(Pattern)), Expr(std::move(Expr)) {}
+  Branch(Pattern::Ptr Patt, AST::Ptr Expr)
+      : Patt(std::move(Patt)), Expr(std::move(Expr)) {}
 };
 
-class Conclassor {
+class Constructor {
 public:
-  using Ptr = std::unique_ptr<Conclassor>;
+  using Ptr = std::unique_ptr<Constructor>;
 
 private:
   const std::string Name;
   const std::vector<std::string> Types;
 
 public:
-  Conclassor(std::string Name, std::vector<std::string> Types)
+  Constructor(std::string Name, std::vector<std::string> Types)
       : Name(std::move(Name)), Types(std::move(Types)) {}
 };
 
@@ -60,20 +93,20 @@ public:
   explicit Int(int V) : Value(V) {}
 };
 
-class Lid : public AST {
+class LID : public AST {
 private:
   const std::string ID;
 
 public:
-  explicit Lid(std::string ID) : ID(std::move(ID)) {}
+  explicit LID(std::string ID) : ID(std::move(ID)) {}
 };
 
-class Uid : public AST {
+class UID : public AST {
 private:
   const std::string ID;
 
 public:
-  explicit Uid(std::string ID) : ID(std::move(ID)) {}
+  explicit UID(std::string ID) : ID(std::move(ID)) {}
 };
 
 class Binop : public AST {
@@ -128,14 +161,14 @@ public:
       : Constr(std::move(Constr)), Params(std::move(Params)) {}
 };
 
-class Let : public Definition {
+class Fn : public Definition {
 private:
   const std::string Name;
   const std::vector<std::string> Params;
   const AST::Ptr Body;
 
 public:
-  Let(std::string Name, std::vector<std::string> Params, AST::Ptr Body)
+  Fn(std::string Name, std::vector<std::string> Params, AST::Ptr Body)
       : Name(std::move(Name)), Params(std::move(Params)),
         Body(std::move(Body)) {}
 };
@@ -143,10 +176,12 @@ public:
 class Data : public Definition {
 private:
   const std::string Name;
-  const std::vector<Conclassor::Ptr> Conclassors;
+  const std::vector<Constructor::Ptr> Constructors;
 
 public:
-  Data(std::string Name, std::vector<Conclassor::Ptr> Conclassors)
-      : Name(std::move(Name)), Conclassors(std::move(Conclassors)) {}
+  Data(std::string Name, std::vector<Constructor::Ptr> Constructors)
+      : Name(std::move(Name)), Constructors(std::move(Constructors)) {}
 };
 } // namespace swallow::ast
+
+#endif
