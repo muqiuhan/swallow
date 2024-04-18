651ebf9488646d0a77acfd7a1841785956be5445
Muqiu Han <muqiu-han@outlook.com>
2024-04-18 11:48:51 +0800

    [swallow]: update project build file

--- compiler/xmake.lua
+++ compiler/xmake.lua
@@ -4,18 +4,10 @@ set_xmakever("2.8.1")
 
 includes("../base")
 
-add_packages("libc++")
-
 target("swc")
     set_kind("binary")
     set_languages("c++20")
     
     add_files("*.cpp", "**/*.cpp")
     add_includedirs(".", "ast", "type", "lexer", "parser", "diagnostics", "utils")
-    add_cxxflags("clang::-stdlib=libc++")
     add_deps("base")
-    add_packages("libc++")
-
-    after_build(function (_)
-        os.run("sh after_build.sh")
-    end)
