76dbbfef0fe168d90f801d28ba5b1be351450932
Muqiu Han <muqiu-han@outlook.com>
2024-04-23 19:19:27 +0800

    [swallow]: set toolchain in  and

--- make.ml
+++ make.ml
@@ -1,13 +1,12 @@
 #!/bin/env ocaml
-
 let _ = 
   begin
     match Sys.argv |> Array.to_list with
     | _ :: "fmt" :: _ -> "find . -iname '*.h' -o -iname '*.cpp' -o -iname '*.hpp' -o -iname '*.hh' | xargs clang-format -i"
     | _ :: "check" :: _ -> "xmake check clang.tidy --configfile=.clang-tidy"
     | _ :: "fix" :: _ -> "xmake check clang.tidy --configfile=.clang-tidy --fix"
-    | _ :: "build" :: _ -> "xmake f -m debug && xmake build -w"
-    | _ :: "release" :: _ -> "xmake f -m release && xmake build -w"
+    | _ :: "build" :: _ -> "xmake f --toolchain=gcc -m debug && xmake build -w"
+    | _ :: "release" :: _ -> "xmake f --toolchain=gcc -m release && xmake build -w"
     | _ :: "build.win" :: _ -> "xmake f --toolchain=mingw -m debug && xmake build -w"
     | _ :: "release.win" :: _ -> "xmake f --toolchain=mingw -m release && xmake build -w"
     | _ -> ""
