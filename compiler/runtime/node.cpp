ea58bfc12d7b3f7c603d7756462d52b9885cf98f
Muqiu Han <muqiu-han@outlook.com>
2024-06-12 17:14:49 +0800

    [compiler]: add runtime node implementation

---
+++ compiler/runtime/node.cpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2023 Muqiu Han
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//     notice,
+//       this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Swallow nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "node.h"
+#include "utils/panic/panic.hpp"
+
+namespace swallow::compiler::runtime::node
+{
+  [[nodiscard]] auto Base::Allocate() noexcept -> class Base *
+  {
+    auto *node = reinterpret_cast<class Base *>(malloc(sizeof(class Application)));
+
+    if (nullptr == node)
+      utils::Panic("ICE: Cannot allocate Base node");
+    else
+      return node;
+  }
+
+  [[nodiscard]] auto Application::Allocate(
+    const class Base *Left, const class Base *Right) noexcept -> class Application *
+  {
+    auto *node = reinterpret_cast<class Application *>(Base::Allocate());
+    node->Base.Tag = Tag::APPLICATION;
+    node->Left = Left;
+    node->Right = Right;
+    return node;
+  }
+
+  [[nodiscard]] auto Int::Allocate(int32_t Value) noexcept -> class Int *
+  {
+    auto *node = reinterpret_cast<class Int *>(Base::Allocate());
+    node->Base.Tag = Tag::INT;
+    node->Value = Value;
+    return node;
+  }
+
+  [[nodiscard]] auto Global::Allocate(
+    const std::function<void(class Stack *)> &Function,
+    int32_t                                   Arity) noexcept -> class Global *
+  {
+    auto *node = reinterpret_cast<class Global *>(Base::Allocate());
+    node->Base.Tag = Tag::GLOBAL;
+    node->Arity = Arity;
+    node->Function = Function;
+    return node;
+  }
+
+  [[nodiscard]] auto Ind::Allocate(const class Base *Next) noexcept -> class Ind *
+  {
+    auto *node = reinterpret_cast<class Ind *>(Base::Allocate());
+    node->Base.Tag = Tag::IND;
+    node->Next = Next;
+    return node;
+  }
+
+} // namespace swallow::compiler::runtime::node
