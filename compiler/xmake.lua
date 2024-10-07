457a6af0b52174a826c4b746ca6cbbfdaa1e6e99
Muqiu Han <muqiu-han@outlook.com>
2024-10-07 16:14:08 +0800

    [compiler]: Reorganize compiler project
      - Cancel the use of static library split modules.

--- compiler/xmake.lua
+++ compiler/xmake.lua
@@ -1,6 +1,5 @@
-set_project("swallow")
-set_version("0.0.1")
-set_xmakever("2.8.1")
+add_requires("tl_optional", "tl_expected", "spdlog")
+add_requires("llvm", { system = true })
 
 target("swc")
     set_kind("static")
@@ -8,5 +7,18 @@ target("swc")
 
     add_files("*.cpp", "**/*.cpp")
     add_includedirs(".")
+    add_packages("tl_optional", "tl_expected", "spdlog", "llvm")
     add_ldflags("clang::-stdlib=libc++")
     add_cxxflags("clang::-stdlib=libc++")
+
+    before_build(function () 
+        local command = "flex -o $(projectdir)/compiler/lexer/flex_lexer.cpp $(projectdir)/compiler/lexer/lexer.l"
+        printf("[ RUN]: %s\n", command)
+        os.run(command)
+    end)
+
+    before_build(function ()
+        local command = "bison -o $(projectdir)/compiler/parser/bison_parser.cpp -d $(projectdir)/compiler/parser/parser.y"
+        printf("[ RUN]: %s\n", command)
+        os.run(command)
+    end)
